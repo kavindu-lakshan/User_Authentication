af66c6933615faf35b3976be12327b22b5efe957
Kavindu Lakshan <kavindulakshan26@gmail.com>
2021-08-24 00:41:57 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { connect } = require("mongoose");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
+const app = express();
+dotenv.config();
+connectDB();
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/footer";
+import LandingPage from "./Screens/LandingPage/LandingPage";
+import HomePage from "./Screens/HomePage/HomePage";
+import { BrowserRouter, Route } from "react-router-dom";
+import LoginScreen from "./Screens/LoginScreen/LoginScreen";
+import RegisterScreen from "./Screens/RegisterScreen/RegisterScreen";
+import ProfileScreen from "./Screens/ProfileScreen/ProfileScreen";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Route path="/" component={LandingPage} exact />
+        <Route path="/login" component={LoginScreen} />
+        <Route path="/profile" component={ProfileScreen} />
+        <Route path="/register" component={RegisterScreen} />
+        <Route path="/Home" component={() => <HomePage />} />
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+export default App;
